da71ce17011aff902c283d7c489c8d2b71490444
Ramon_Trigo <ramtrigo@gmail.com>
2024-02-27 11:38:23 -0300

    envio

---
+++ components/DButton/DButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+
+import { styles } from './DButtonstyles';
+
+export function DButton() {
+    return (
+        <View style={styles.container}>
+            <Button
+              onPress={()=>{console.log("olá")}}
+              title='Click Aqui'
+              accessibilityLabel='click em min'
+
+            />
+        </View>
+    );
+}

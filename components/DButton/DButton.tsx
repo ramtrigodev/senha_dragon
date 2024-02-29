70514f5fc64927096a49ae8dcafd8157e47b864b
Ramon Trigo <ramtrigo@gmail.com>
2024-02-29 18:10:09 -0300

    envio

--- components/DButton/DButton.tsx
+++ components/DButton/DButton.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, View, Text, Pressable } from 'react-native';
 
 import { styles } from './DButtonstyles';
+import { DTextInput } from '../DTextInput/DTextInput';
 
 export function DButton() {
+    const [pass, setPass] = useState('')
+
+    function handleGenerateButton() {
+        setPass('olá')
+    }
     return (
         <>
-           
+            <DTextInput pass={pass} />
             <Pressable
-                onPress={() => console.log("pressionado")}
-              style={styles.button}
-           >
+                onPress= {handleGenerateButton}
+                style={styles.button}
+            >
                 <Text style={styles.text}> GERADOR </Text>
             </Pressable>
 
--- components/DTextInput/DTextInput.tsx
+++ components/DTextInput/DTextInput.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { TextInput } from "react-native";
 import { styles } from './DTextInputStyles';
 
-export function DTextInput() {
+interface  DTextInputProps {
+    pass:string
+}
+export function DTextInput(props: DTextInputProps) {
     return (
 
         <TextInput
         style={styles.inputer}
             placeholder='SENHA'
+            value ={ props.pass}
             
         ></TextInput>
 
